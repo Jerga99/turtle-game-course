56bb60f27d65ee6af1444825f7e82cb2d70c0379
Filip Jerga <jerga99@gmail.com>
2023-11-16 13:31:23 +0000

    Inherit Turtle

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# from turtle import *
-# import turtle
 from turtle import Screen
 import random
 from player import Player
 
 _player = Player()
-
 
 screen = Screen()
 screen.setup(800, 600)
@@ -15,8 +12,8 @@ screen.bgcolor('#000000')
 for i in range(20):
     distance = random.randint(50, 100)
     angle = random.randint(0, 90)
-    _player.t.left(angle)
-    _player.t.fd(distance)
+    _player.left(angle)
+    _player.fd(distance)
 
 
 screen.mainloop()
--- player.py
+++ player.py
@@ -1,50 +1,9 @@
 
 from turtle import Turtle
 
-class Player:
-    def __init__(self) -> None:
-        self.t = Turtle()
-        self.t.color('#3ff6ff')
-        self.t.speed(3)
-
-
-class MyTurtle:
-    def __init__(self) -> None:
-        self.a = 10
-        self.b = 20
-        self.c = 30
-
-    def method_1(self):
-        pass
-
-    def method_2(self):
-        pass
-
-    def method_3(self):
-        pass
-
-    def method_4(self):
-        pass
-
-
-class MyPlayer(MyTurtle):
+class Player(Turtle):
     def __init__(self) -> None:
         super().__init__()
-        self.x = 40
-        self.a = 100
-        self.b = 200
-
-    def method_X(self):
-        pass
-
-    def method_Y(self):
-        pass
-
-    def method_Z(self):
-        pass
-
-player = MyPlayer()
-
-print(player.a)
-print(player.b)
-print(player.x)
+        self.color('#3ff6ff')
+        self.shape('turtle')
+        self.speed(3)
