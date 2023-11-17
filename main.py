0008045073b9f7e0480773aad434bf9dbfc893bf
Filip Jerga <jerga99@gmail.com>
2023-11-17 09:02:03 +0000

    Move to each side

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ from window import Window
 player = Player()
 window = Window()
 
-# def move_up(distance: int):
-#     def move():
-#         player.forward(distance)
-
-#     return move
-
-# window.screen.onkeypress(move_up(400), 'w')
-
 def move_up():
     new_y = player.ycor() + 20
     player.setpos(player.xcor(), new_y)
 
+def move_down():
+    new_y = player.ycor() - 20
+    player.setpos(player.xcor(), new_y)
+
+def move_left():
+    new_x = player.xcor() - 20
+    player.setpos(new_x, player.ycor())
+
+def move_right():
+    new_x = player.xcor() + 20
+    player.setpos(new_x, player.ycor())
+
 window.screen.onkeypress(move_up, 'w')
+window.screen.onkeypress(move_down, 's')
+window.screen.onkeypress(move_left, 'a')
+window.screen.onkeypress(move_right, 'd')
 
 window.screen.listen()
 window.screen.mainloop()
