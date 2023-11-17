ff67329c2a8d7a5edcaab332dea27053b3f3161c
Filip Jerga <jerga99@gmail.com>
2023-11-17 10:40:50 +0000

    Player update

--- main.py
+++ main.py
@@ -6,31 +6,29 @@ def start_game():
     player = Player()
     window = Window()
 
-    def move_up():
-        new_y = player.ycor() + g.DISTANCE_STEP
-        player.setpos(player.xcor(), new_y)
+    # def move_up():
+    #     new_y = player.ycor() + g.DISTANCE_STEP
+    #     player.setpos(player.xcor(), new_y)
 
-    def move_down():
-        new_y = player.ycor() - g.DISTANCE_STEP
-        player.setpos(player.xcor(), new_y)
+    # def move_down():
+    #     new_y = player.ycor() - g.DISTANCE_STEP
+    #     player.setpos(player.xcor(), new_y)
 
-    def move_left():
-        new_x = player.xcor() - g.DISTANCE_STEP
-        player.setpos(new_x, player.ycor())
+    # def move_left():
+    #     new_x = player.xcor() - g.DISTANCE_STEP
+    #     player.setpos(new_x, player.ycor())
 
-    def move_right():
-        new_x = player.xcor() + g.DISTANCE_STEP
-        player.setpos(new_x, player.ycor())
+    # def move_right():
+    #     new_x = player.xcor() + g.DISTANCE_STEP
+    #     player.setpos(new_x, player.ycor())
 
-    window.screen.onkeypress(move_up, 'w')
-    window.screen.onkeypress(move_down, 's')
-    window.screen.onkeypress(move_left, 'a')
-    window.screen.onkeypress(move_right, 'd')
+    # window.screen.onkeypress(move_up, 'w')
+    # window.screen.onkeypress(move_down, 's')
+    # window.screen.onkeypress(move_left, 'a')
+    # window.screen.onkeypress(move_right, 'd')
 
     def update_loop():
-
-        move_right()
-
+        player.update()
         window.screen.ontimer(update_loop, 10)
 
     update_loop()
--- player.py
+++ player.py
@@ -8,3 +8,7 @@ class Player(Turtle):
         self.shape('turtle')
         self.speed(0)
         self.penup()
+        self.direction: tuple[float, float] = (1,0)
+
+    def update(self):
+        self.setpos(self.pos() + self.direction)
