bb3b022132486a7f5c4fb4c0d64211d4c9493bc0
Filip Jerga <jerga99@gmail.com>
2023-11-17 08:55:46 +0000

    Move up with set position

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ from window import Window
 player = Player()
 window = Window()
 
+# def move_up(distance: int):
+#     def move():
+#         player.forward(distance)
+
+#     return move
+
+# window.screen.onkeypress(move_up(400), 'w')
+
 def move_up():
-    player.forward(20)
+    new_y = player.ycor() + 20
+    player.setpos(player.xcor(), new_y)
 
 window.screen.onkeypress(move_up, 'w')
 
