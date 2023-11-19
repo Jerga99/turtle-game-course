a2f152ecdb3e0740e7d2526079139fdbdc677743
Filip Jerga <jerga99@gmail.com>
2023-11-19 14:30:21 +0000

    Watched key

--- main.py
+++ main.py
@@ -2,31 +2,37 @@
 from player import Player
 from window import Window
 from game_time import GameTime
+from watched_key import WatchedKey
 import globals as g
 
 def start_game():
     player = Player()
     window = Window()
 
-    def move_up():
-        player.set_direction(0, 1)
+    # def move_up():
+    #     player.set_direction(0, 1)
 
-    def move_down():
-        player.set_direction(0, -1)
+    # def move_down():
+    #     player.set_direction(0, -1)
 
-    def move_left():
-        player.set_direction(-1, 0)
+    # def move_left():
+    #     player.set_direction(-1, 0)
 
-    def move_right():
-        player.set_direction(1, 0)
+    # def move_right():
+    #     player.set_direction(1, 0)
 
-    window.screen.onkeypress(move_up, 'w')
-    window.screen.onkeypress(move_down, 's')
-    window.screen.onkeypress(move_left, 'a')
-    window.screen.onkeypress(move_right, 'd')
+    # onkeypress(move_up, 'w')
+    # onkeypress(move_down, 's')
+    # onkeypress(move_left, 'a')
+    # onkeypress(move_right, 'd')
+
+    space = WatchedKey('space')
+    w = WatchedKey('w')
 
     def update_loop():
         GameTime.process_time()
+
+        print(f'W is down: {w.down}')
 
         player.update()
         window.screen.update()
